7c792083b2b7697d0828f4972e14d7792203517d
Rimu Roy <titashroy112@gmail.com>
2025-09-29 00:34:03 +0600

    BMP180

---
+++ BMP 180 sensor/BMP180/BMP180.ino
@@ -0,0 +1,43 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#define seaLevelPressure_hpa 1013.25
+
+Adafruit_BMP085 bmp;
+
+void setup() {
+  Serial.begin(9600);
+  if(!bmp.begin())
+  {
+    Serial.println("Could not find a valid BMP085 sensor, chech wiring!");
+    while(1) {}
+    
+  }
+
+}
+
+void loop() {
+ Serial.print("Temperature = ");
+ Serial.print(bmp.readTemperature());
+ Serial.println(" *C");
+
+ Serial.print("Pressure = ");
+ Serial.print(bmp.readPressure());
+ Serial.println(" Pa");
+
+ Serial.print("Altitude = ");
+ Serial.print(bmp.readAltitude());
+ Serial.println(" meters");
+
+ Serial.print("Pressure at seaLevel (calculated) = ");
+ Serial.print(bmp.readSealevelPressure());
+ Serial.println(" Pa");
+
+ Serial.print("Real altitude = ");
+ Serial.print(bmp.readAltitude(seaLevelPressure_hpa * 100));
+ Serial.println(" meters");
+
+ Serial.println();
+ delay(500);
+ 
+
+}
